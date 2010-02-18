3c6c40215efb8ad4fd7fbac74c512eb4bc659e56
Peter Miller <peter.h.miller@gmail.com>
2010-02-17 20:09:46 -0600

    initial commit

---
+++ AsyncWorker/AsyncUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Phm.AsyncWorker
+{
+    public static class AsyncUtil
+    {
+        public static void ExecuteAsync(List<Action> actions, Action<Exception> exceptionHandler)
+        {
+            foreach (var action in actions)
+            {
+                var actionToExecute = action;
+                ThreadPool.QueueUserWorkItem(state =>
+                {
+                    try
+                    {
+                        actionToExecute();
+                    }
+                    catch (Exception e)
+                    {
+                        exceptionHandler(e);
+                    }
+                }, null);
+            }
+        }
+    }
+}
---
+++ AsyncWorker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Phm.AsyncWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            List<Action> actionsToExecuteAsync = new List<Action>() 
+            {
+                () =>  System.Console.WriteLine("executed action 1"),
+                () =>  System.Console.WriteLine("executed action 2"),
+                () =>  System.Console.WriteLine("exception time: {0}", 1/i),
+                () =>  System.Console.WriteLine("executed action 4")
+            };
+            AsyncUtil.ExecuteAsync(actionsToExecuteAsync, HandleException);
+            System.Console.ReadLine();
+        }
+
+        static void HandleException(Exception e)
+        {
+            System.Console.WriteLine("exception: {0}", e);
+        }
+    }
+}
